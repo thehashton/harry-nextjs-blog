0892ebfd7a75715c69f52ef48abb5fd443a0e044
hghazni <hghazni@gmail.com>
2021-12-02 03:03:29 +0000

    Adds blog animation

--- components/BlogPost/BlogPost.tsx
+++ components/BlogPost/BlogPost.tsx
@@ -1,15 +1,26 @@
+import { motion } from 'framer-motion';
 import React from 'react';
 import { Container } from '../../styles/Global.styled';
 import HeadPost from '../HeadPost/'
 
 const BlogPost = ({ children, meta}) => {
   return (
-    <>
-      <Container>
-        <HeadPost meta={meta} isBlogPost />
-        <article>{children}</article>
-      </Container>
-    </>
+    <motion.div
+        initial="initial"
+        animate="animate"
+        variants={{
+          initial: {
+            opacity: 0,
+          },
+          animate: {
+            opacity: 1,
+          },}}
+      >
+        <Container>
+          <HeadPost meta={meta} isBlogPost />
+          <article style={{fontSize: '1.2rem'}}>{children}</article>
+        </Container>
+      </motion.div>
   )
 }
 
