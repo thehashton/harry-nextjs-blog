ad42e249b80e7394cbfd33632ddf1101c71ee6de
hghazni <hghazni@gmail.com>
2021-12-06 01:06:22 +0000

    Adds space to PP title

--- pages/PrivacyPolicy/PrivacyPolicy.tsx
+++ pages/PrivacyPolicy/PrivacyPolicy.tsx
@@ -5,7 +5,7 @@ import Layout from "../../components/Layout";
 const PrivacyPolicy: NextPage = () => {
     return (
     <Layout pageTitle="My Mission" description="My mission">
-        <h1>PrivacyPolicy</h1>
+        <h1>Privacy Policy</h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque earum pariatur expedita suscipit? Nobis quis voluptate tenetur aliquid, reiciendis magnam accusamus officiis ex dolorem porro unde nulla nam cupiditate. Totam.</p>
     </Layout>
     )
