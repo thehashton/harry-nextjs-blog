2ccab1a2699d4866a49a830895cf3515dafa4522
hghazni <hghazni@gmail.com>
2022-08-05 16:32:58 +0100

    adds common file for meta data

---
+++ common.ts
@@ -0,0 +1,11 @@
+const common = {
+  name: 'Harry Ashton',
+  role: 'UI/Accessibility engineer',
+  social: {
+    twitter: 'https://twitter.com/TheHashton',
+    github: 'https://github.com/thehashton',
+    medium: 'https://medium.com/@thehashton'
+  }
+}
+
+export default common;
